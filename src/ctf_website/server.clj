9b6b81032e6114687aa89339c63cc23962844767
David Barksdale <amatus.amongus@gmail.com>
2012-04-04 20:56:29 -0500

    Configure https.

--- src/ctf_website/server.clj
+++ src/ctf_website/server.clj
@@ -3,9 +3,23 @@
 
 (server/load-views "src/ctf_website/views/")
 
+(defn redirect-https
+  [handler]
+  (fn [request]
+    (if (= :http (:scheme request))
+      (ring.util.response/redirect
+        (str "https://" (:server-name request) (:uri request)))
+      (handler request))))
+
+(server/add-middleware redirect-https)
+
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "80"))]
     (server/start port {:mode mode
+                        :jetty-options {:ssl? true
+                                        :ssl-port 443
+                                        :keystore "/home/ctf/ctf-website/key_crt.jks"
+                                        :key-password "password"}
                         :ns 'ctf-website})))
 
