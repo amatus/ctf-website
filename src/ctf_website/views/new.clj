58d95efffff28e28d4f8db885b7abe7613728740
David Barksdale <amatus.amongus@gmail.com>
2012-04-04 21:29:02 -0500

    Improved new account security.
    Since chpasswd takes multiple username:password lines
    it was possible to change the password of any account:
    curl -data "username=attacker&password=%0aroot:omghax" -k https://ctf/new

--- src/ctf_website/views/new.clj
+++ src/ctf_website/views/new.clj
@@ -19,19 +19,27 @@
       [:input {:type "submit"
                :value "Create"}]]]))
 
+(def fail
+  (common/layout
+    [:p "Try a username that doesn't suck"]))
+
+(def good
+  (ring.util.response/redirect "login"))
+
 (defpage
   [:post "/new"] {:keys [username password]}
   (let [adduser (.start (ProcessBuilder. (list "adduser" username)))
         _ (.close (.getOutputStream adduser))
         retval (.waitFor adduser)]
     (if (not (= 0 retval))
-      (common/layout
-        [:p "Try a username that doesn't suck"]
-        [:p (str "result: " retval)])
+      fail ;; this seems to take care of usernames containing : or \n
       (let [chpasswd (.start (ProcessBuilder. (list "chpasswd")))
             out (.getOutputStream chpasswd)
-            _ (.write out (.getBytes (str username ":" password) "UTF-8"))
+            userpass (.getBytes (str username ":" password) "UTF-8")
+            ;; chpasswd seems to only care about \n, though I only tested
+            ;; \n and \r and \0.
+            userpass (remove #(= 0x0a %) userpass)
+            _ (.write out (into-array Byte/TYPE userpass))
             _ (.close out)
             retval (.waitFor chpasswd)]
-        (common/layout
-          [:p (str "result: " retval)])))))
+        good))))
