5e66967a341052649226051731c5fb84b9592555
David Barksdale <amatus.amongus@gmail.com>
2012-04-05 22:11:59 -0500

    Throttle account creation.

--- src/ctf_website/views/new.clj
+++ src/ctf_website/views/new.clj
@@ -1,6 +1,7 @@
 (ns ctf-website.views.new
   (:require [ctf-website.views.common :as common])
-  (:import java.lang.ProcessBuilder)
+  (:import java.lang.ProcessBuilder
+           [java.util.concurrent Executors TimeUnit])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
@@ -21,25 +22,41 @@
 
 (def fail
   (common/layout
-    [:p "Try a username that doesn't suck"]))
+    [:p "Try a username that doesn't suck."]))
+
+(def throttled
+  (common/layout
+    [:p "Woah woah woah! Let's not all talk at once."]))
 
 (def good
   (ring.util.response/redirect "login"))
 
+(defonce *barrier* (ref true))
+(defonce *timer* (Executors/newScheduledThreadPool 1))
+
 (defpage
   [:post "/new"] {:keys [username password]}
-  (let [adduser (.start (ProcessBuilder. (list "adduser" username)))
-        _ (.close (.getOutputStream adduser))
-        retval (.waitFor adduser)]
-    (if (not (= 0 retval))
-      fail ;; this seems to take care of usernames containing : or \n
-      (let [chpasswd (.start (ProcessBuilder. (list "chpasswd")))
-            out (.getOutputStream chpasswd)
-            userpass (.getBytes (str username ":" password) "UTF-8")
-            ;; chpasswd seems to only care about \n, though I only tested
-            ;; \n and \r and \0.
-            userpass (remove #(= 0x0a %) userpass)
-            _ (.write out (into-array Byte/TYPE userpass))
-            _ (.close out)
-            retval (.waitFor chpasswd)]
-        good))))
+  (let [throttle (ref true)]
+    (dosync
+      (when (deref *barrier*)
+        (ref-set throttle false)
+        (ref-set *barrier* false)))
+    (if (deref throttle)
+      throttled
+      (let [_ (.schedule *timer* #(dosync (ref-set *barrier* true))
+                         1 (TimeUnit/SECONDS))
+            adduser (.start (ProcessBuilder. (list "adduser" username)))
+            _ (.close (.getOutputStream adduser))
+            retval (.waitFor adduser)]
+        (if (not (= 0 retval))
+          fail ;; this seems to take care of usernames containing : or \n
+          (let [chpasswd (.start (ProcessBuilder. (list "chpasswd")))
+                out (.getOutputStream chpasswd)
+                userpass (.getBytes (str username ":" password) "UTF-8")
+                ;; chpasswd seems to only care about \n, though I only tested
+                ;; \n and \r and \0.
+                userpass (remove #(= 0x0a %) userpass)
+                _ (.write out (into-array Byte/TYPE userpass))
+                _ (.close out)
+                retval (.waitFor chpasswd)]
+            good))))))
